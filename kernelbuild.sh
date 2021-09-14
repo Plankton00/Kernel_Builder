ee47b3f5bfe663e0a5f6513ded06c75def9ebf2d
Plankton00 <85942162+Plankton00@users.noreply.github.com>
2021-09-14 11:13:54 +0700

    Update kernelbuild.sh

--- kernelbuild.sh
+++ kernelbuild.sh
@@ -18,12 +18,12 @@
 echo "|| Downloading few Dependecies . . .||"
 # Kernel Sources
 git clone --depth=1 $KERNEL_SOURCE $KERNEL_BRANCH $DEVICE_CODENAME
-git clone --depth=1 https://github.com/cbendot/elastics-toolchain Elastics # Elastics set as Clang Default
+git clone --depth=1 https://github.com/Plankton00/Predator_Clang_14 Predator # Predator set as Clang Default
 
 # Main Declaration
 KERNEL_ROOTDIR=$(pwd)/$DEVICE_CODENAME # IMPORTANT ! Fill with your kernel source root directory.
 DEVICE_DEFCONFIG=$DEVICE_DEFCONFIG # IMPORTANT ! Declare your kernel source defconfig file here.
-CLANG_ROOTDIR=$(pwd)/Elastics # IMPORTANT! Put your clang directory here.
+CLANG_ROOTDIR=$(pwd)/Predator # IMPORTANT! Put your clang directory here.
 export KBUILD_BUILD_USER=$BUILD_USER # Change with your own name or else.
 export KBUILD_BUILD_HOST=$BUILD_HOST # Change with your own hostname.
 
